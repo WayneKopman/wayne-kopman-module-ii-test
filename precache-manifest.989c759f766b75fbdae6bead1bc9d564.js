4c3c28a7f6feeaf2bfa6bb880a6da1db498186b7
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-16 12:10:40 +0100

    Updates

---
+++ precache-manifest.989c759f766b75fbdae6bead1bc9d564.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763bfe5c18ec8d092eed94c0042489d9",
+    "url": "/wayne-kopman-module-ii-test/index.html"
+  },
+  {
+    "revision": "c0d63842c24c82d78138",
+    "url": "/wayne-kopman-module-ii-test/static/css/main.6aaad872.chunk.css"
+  },
+  {
+    "revision": "10247116eae4b4f2fdd1",
+    "url": "/wayne-kopman-module-ii-test/static/js/2.436d728d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wayne-kopman-module-ii-test/static/js/2.436d728d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d63842c24c82d78138",
+    "url": "/wayne-kopman-module-ii-test/static/js/main.1d8f8f3c.chunk.js"
+  },
+  {
+    "revision": "d8d0c3b3b262cec8e603",
+    "url": "/wayne-kopman-module-ii-test/static/js/runtime-main.7f0bd56e.js"
+  },
+  {
+    "revision": "ae542c23900e42f0c017b7e62c70ec78",
+    "url": "/wayne-kopman-module-ii-test/static/media/Coffee-Bags-Flags-India.ae542c23.png"
+  },
+  {
+    "revision": "10e65949346ee7983ea245f88217afa1",
+    "url": "/wayne-kopman-module-ii-test/static/media/SocietyCoffee-Logo-Web.10e65949.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/wayne-kopman-module-ii-test/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/wayne-kopman-module-ii-test/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/wayne-kopman-module-ii-test/static/media/times-solid.2c5552a1.svg"
+  }
+]);
